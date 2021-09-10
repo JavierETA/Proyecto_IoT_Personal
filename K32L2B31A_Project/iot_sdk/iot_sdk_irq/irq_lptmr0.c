a0d1bf6819abef2bf0f2cff1f353aa40daef7bbc
Javier Tobon <javier.elias.tobon.ayubb30@gmail.com>
2021-09-10 16:57:06 -0500

    Se crea librerias para controlar interrupcion del lptmr0
    adicional se elimina desde MCUXpresso fsl_adapter_lptmr.c para corregir error de cruze de declaraciones.

---
+++ K32L2B31A_Project/iot_sdk/iot_sdk_irq/irq_lptmr0.c
@@ -0,0 +1,56 @@
+/*! @file : irq_lptmr0.c
+ * @author  JAVIER ELIAS TOBON AYUBB
+ * @version 1.0.0
+ * @date    10/09/2021
+ * @brief   Driver para interrupcion cada segundo
+ * @details
+ *
+*/
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "irq_lptmr0.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+volatile bool lptmr0_irqCounter = false;
+
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
+ /* LPTMR0_IRQn interrupt handler cada 1 segundo */
+void LPTMR0_IRQHANDLER(void) {
+  uint32_t intStatus;
+  /* Reading all interrupt flags of status register */
+  intStatus = LPTMR_GetStatusFlags(LPTMR0_PERIPHERAL);
+  LPTMR_ClearStatusFlags(LPTMR0_PERIPHERAL, intStatus);
+
+  /* Place your code here */
+  lptmr0_irqCounter = true;
+  /* Add for ARM errata 838869, affects Cortex-M4, Cortex-M4F
+     Store immediate overlapping exception return operation might vector to incorrect interrupt. */
+  #if defined __CORTEX_M && (__CORTEX_M == 4U)
+    __DSB();
+  #endif
+}
+
+/*******************************************************************************
+ * Public Source Code
+ ******************************************************************************/
+
